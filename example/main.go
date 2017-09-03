1bd76c29d4eb59e2c9ff6dd107beae670b1cb537
Kelsey Hightower <kelsey.hightower@gmail.com>
2017-09-02 21:10:07 -0700

    update license headers

--- example/main.go
+++ example/main.go
@@ -1,3 +1,7 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
 package main
 
 import (
